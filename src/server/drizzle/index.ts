13ce43fc3afadaaf4f568ccec57e2abe4a38c925
Oliver Gilan <github@olivergilan.com>
2024-12-04 22:53:15 -0800

    update readme

--- src/server/drizzle/index.ts
+++ src/server/drizzle/index.ts
@@ -1,8 +1,23 @@
 import { config } from "dotenv";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { join } from "path";
 
 config({ path: ".env" }); // or .env.local
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle({ client });
+
+// const checkMigrations = async (db: ReturnType<typeof drizzle>) => {
+//   try {
+//     await migrate(db, {
+//       migrationsFolder: join(process.cwd(), "src/server/drizzle/migrations"),
+//     });
+//     console.log("migrations up to date");
+//   } catch (error) {
+//     throw new Error(`Failed to run database migrations: ${error instanceof Error ? error.message : String(error)}`);
+//   }
+// };
+
+// checkMigrations(db);
